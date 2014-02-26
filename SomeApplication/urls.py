6779033c8f289b31b892a363f1b0dc1bef6e7bb6
killbotlogic <me@killbotlogic.com>
2014-02-26 19:39:28 +1100

    More work

--- SomeApplication/urls.py
+++ SomeApplication/urls.py
@@ -10,9 +10,11 @@ urlpatterns = patterns('',
     url(r'^login/$', views.login, name='login'),
     url(r'^login_post/$', views.login_post, name='login_post'),
     # ex: /SomeApplication/5/
-    url(r'^(?P<company_id>\d+)/$', views.dashboard, name='dashboard'),
+    url(r'^(?P<person_id>\d+)/$', views.dashboard, name='dashboard'),
     # ex: /SomeApplication/dashboard/5/
-    url(r'^dashboard/(?P<company_id>\d+)/$', views.dashboard, name='dashboard'),
+    url(r'^dashboard/(?P<person_id>\d+)/$', views.dashboard, name='dashboard'),
+    url(r'^report/(?P<report_type>[\w])/(?P<person_id>\d+)/(?P<company>[\w]{3})/$', views.report, name='report'),
+    url(r'^report_post/$', views.report_post, name='report_post'),
     # ex: /SomeApplication/5/results/
     #url(r'^(?P<company_id>\d+)/results/$', views.results, name='results'),
     # ex: /SomeApplication/5/vote/
--- SomeApplication/views.py
+++ SomeApplication/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from SomeApplication.models import OptionMatrix, Incident, Person
 from django.contrib.auth import authenticate
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+
 
 def index(request):
     company_list = OptionMatrix.objects.order_by('-company')[:5]
@@ -18,30 +19,34 @@ def index(request):
 #    context = {'company_list': company_list}
 #    return render(request, 'SomeApplication/index.html', context)
 
-def dashboard(request, company_id):
-    try:
-        company = OptionMatrix.objects.get(pk=company_id)
-    except:
-        raise Http404
+def dashboard(request, person_id):
+    person = Person.objects.filter(id=person_id).first()
 
-    boxes = {'A': company.option_a,
-             'B': company.option_b,
-             'C': company.option_c,
-             'D': company.option_d,
-             'E': company.option_e,
-             'F': company.option_f,
-             'G': company.option_g,
-             'H': company.option_h,
-             'I': company.option_i,
-             'J': company.option_j,
-             'K': company.option_k,
-             'L': company.option_l,
-             'M': company.option_m,
-             'N': company.option_n,
-             'O': company.option_o,
-             'P': company.option_p}
+    companies = OptionMatrix.objects.filter(person__id=person_id).all()
 
-    return render(request, 'SomeApplication/dashboard.html', {'company': company, 'boxes': iter(sorted(boxes.items()))})
+    tbl = []
+    for x in companies:
+        coll = {'A': x.option_a,
+                   'B': x.option_b,
+                   'C': x.option_c,
+                   'D': x.option_d,
+                   'E': x.option_e,
+                   'F': x.option_f,
+                   'G': x.option_g,
+                   'H': x.option_h,
+                   'I': x.option_i,
+                   'J': x.option_j,
+                   'K': x.option_k,
+                   'L': x.option_l,
+                   'M': x.option_m,
+                   'N': x.option_n,
+                   'O': x.option_o,
+                   'P': x.option_p}
+
+        tbl.append(iter(sorted(coll.items())))
+        x.boxes = iter(sorted(coll.items()))
+
+    return render(request, 'SomeApplication/dashboard.html', {'companies': companies, 'table': tbl, 'person': person})
 
 # is equal to
 #def dashboard(request, company_id):
@@ -54,8 +59,8 @@ def dashboard(request, company_id):
 def login(request):
     return render(request, 'SomeApplication/login.html')
 
-def login_post(request):
 
+def login_post(request):
     email = request.POST['user']
     password = request.POST['pass']
 
@@ -63,14 +68,19 @@ def login_post(request):
 
     if p is not None:
         if password == p.password:
-            c = OptionMatrix.objects.first()
-            return HttpResponseRedirect(reverse('SomeApplication:dashboard', args=(c.id,)))
+            return HttpResponseRedirect(reverse('SomeApplication:dashboard', args=(p.id,)))
 
-
-
-    messages.add_message(request, messages.constants.INFO, 'Hello world.')
+    messages.add_message(request, messages.constants.INFO, 'Email does not exist on our database')
     return HttpResponseRedirect(reverse('SomeApplication:login'))
     #company_list = OptionMatrix.objects.order_by('-company')[:5]
     #template = loader.get_template('SomeApplication/index.html')
     #context = RequestContext(request, {'company_list': company_list})
     #return HttpResponse(template.render(context))
+
+def report(request, report_type, person_id, company):
+
+    p = Person.objects.filter(id=person_id).first()
+    return render(request, 'SomeApplication/report.html', {'report_type': report_type, 'person': p})
+def report_post(request):
+
+    pass
